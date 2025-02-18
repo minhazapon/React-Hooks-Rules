ee497c63c4685f0334a0728889de5b2cd79c7d57
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-18 01:34:48 -0800

    added file

--- src/hooks/Challenges.jsx
+++ src/hooks/Challenges.jsx
@@ -4,7 +4,18 @@ import { useState } from "react"
 
 function Challenges() {
 
-    const [counter, setCounter] = useState([])
+    const [counter, setCounter] = useState("")
+
+    const handleIncrement = () => {
+        setCounter(counter + 1)
+    }
+    const handleDecrement = () => {
+        setCounter(counter - 1)
+    }
+
+    const handleReset = () => {
+        setCounter("")
+    }
 
 
     return (
@@ -12,16 +23,16 @@ function Challenges() {
             <div className=" border-[1px] border-blue-800  p-10 rounded">
                 <p className=" text-center text-6xl font-bold text-black ">UseState Challenge</p>
                 <div>
-                    <p className=" mt-10 text-xl "> Count: 0 </p>
+                    <p className=" mt-10 text-xl "> Count: {counter} </p>
                     <div className=" mt-10 flex items-center gap-3 ">
                         <p className=" text-xl ">SetValue:</p>
                         <input className=" border-[1px] border-blue-800 h-[50px] w-full rounded-lg p-5 " placeholder="Value" type="text" />
                     </div>
                 </div>
                 <div className=" flex justify-between gap-5 items-center mt-14  ">
-                    <button className=" btn text-white bg-cyan-600 w-[300px] ">Increment</button>
-                    <button className=" btn text-white bg-cyan-600 w-[300px] ">Decrement</button>
-                    <button className=" btn text-white bg-cyan-600 w-[300px] ">Reset</button>
+                    <button onClick={() => handleIncrement()} className=" btn text-white bg-cyan-600 w-[300px] ">Increment</button>
+                    <button onClick={() => handleDecrement()} className=" btn text-white bg-cyan-600 w-[300px] ">Decrement</button>
+                    <button onClick={handleReset} className=" btn text-white bg-cyan-600 w-[300px] ">Reset</button>
                 </div>
             </div>
         </div>
