418149735b9143cf9ed30db9ed8fe7cac2bd1b98
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-18 05:56:13 -0800

    added login form

--- src/registration/Login.jsx
+++ src/registration/Login.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
 
-
 function Login() {
 
     const [email, setEmail] = useState("")
@@ -15,16 +14,11 @@ function Login() {
         console.log(formData)
     }
 
-
     return (
         <div className=" flex justify-center m-20 ">
-
             <div className="flex flex-col w-full p-6 rounded-md sm:p-10 bg-gray-900 text-gray-100">
-
                 <form onSubmit={handleForm} className="space-y-12">
-
                     <div className="space-y-4">
-
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
                             <input type="email"
@@ -36,7 +30,6 @@ function Login() {
                                 required
                                 className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" />
                         </div>
-
                         <div>
                             <div className="flex justify-between mb-2">
                                 <label htmlFor="password" className="text-sm">Password</label>
@@ -51,19 +44,14 @@ function Login() {
                                 placeholder="password"
                                 className="w-full px-3 py-2 border rounded-md border-gray-700 bg-gray-900 text-gray-100" />
                         </div>
-
                     </div>
-
-
                     <div className="space-y-2">
                         <div>
                             <input type="submit" className="w-full px-8 py-3 font-semibold rounded-md bg-violet-400 text-gray-900" value="login" />
                         </div>
                     </div>
-
                 </form>
             </div>
-
         </div>
     )
 }
