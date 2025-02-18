17b4faa5abfecd118dac67fb194a945a170ddc52
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-18 01:28:05 -0800

    added file

--- src/hooks/Challenges.jsx
+++ src/hooks/Challenges.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
 
 
 
 function Challenges() {
 
+    const [counter, setCounter] = useState([])
 
 
     return (
-        <div>
-
+        <div className="m-20 ">
+            <div className=" border-[1px] border-blue-800  p-10 rounded">
+                <p className=" text-center text-6xl font-bold text-black ">UseState Challenge</p>
+                <div>
+                    <p className=" mt-10 text-xl "> Count: 0 </p>
+                    <div className=" mt-10 flex items-center gap-3 ">
+                        <p className=" text-xl ">SetValue:</p>
+                        <input className=" border-[1px] border-blue-800 h-[50px] w-full rounded-lg p-5 " placeholder="Value" type="text" />
+                    </div>
+                </div>
+                <div className=" flex justify-between gap-5 items-center mt-14  ">
+                    <button className=" btn text-white bg-cyan-600 w-[300px] ">Increment</button>
+                    <button className=" btn text-white bg-cyan-600 w-[300px] ">Decrement</button>
+                    <button className=" btn text-white bg-cyan-600 w-[300px] ">Reset</button>
+                </div>
+            </div>
         </div>
     )
 }
