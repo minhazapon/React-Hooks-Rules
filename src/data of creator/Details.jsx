1bf3ab84ba7213ef75063f20324cf3fbc99861ed
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-19 03:29:28 -0800

    added file

--- src/data of creator/Details.jsx
+++ src/data of creator/Details.jsx
@@ -8,8 +8,21 @@ function Details({ creator }) {
     return (
         <>
             <div>
-
-
+                <div className="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://techsteamcenter.com/wp-content/uploads/2023/08/computer-language.webp"
+                            alt="Shoes" />
+                    </figure>
+                    <div className="card-body">
+                        <h2 className="card-title"> name: {name} </h2>
+                        <h2 className="card-title"> name: {technology} </h2>
+                        <p className=" mt-3 ">details: {details} </p>
+                        <p className=" mt-3 ">university: {university} </p>
+                        <p className=" mt-3 ">since: {since} </p>
+                        <p className=" mt-3 ">country: {country} </p>
+                    </div>
+                </div>
             </div>
         </>
     )
