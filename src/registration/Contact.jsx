0e11b436d396a8b6a0ef0db04076542333b7f178
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-18 20:56:37 -0800

    added file

--- src/registration/Contact.jsx
+++ src/registration/Contact.jsx
@@ -2,8 +2,21 @@
 
 function Contact() {
     return (
-        <div>
-
+        <div className=" m-20 ">
+            <div class="p-4 mx-auto max-w-xl bg-white font-[sans-serif] border-[1px]">
+                <h1 class="text-2xl text-gray-800 font-bold text-center">Contact us</h1>
+                <form class="mt-8 space-y-4">
+                    <input required type='text' placeholder='Name'
+                        class="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border focus:border-black focus:bg-transparent text-sm outline-none transition-all" />
+                    <input required type='email' placeholder='Email'
+                        class="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border focus:border-black focus:bg-transparent text-sm outline-none transition-all" />
+                    <input required type='text' placeholder='Subject'
+                        class="w-full py-2.5 px-4 text-gray-800 bg-gray-100 border focus:border-black focus:bg-transparent text-sm outline-none transition-all" />
+                    <textarea required placeholder='Message' rows="4"
+                        class="w-full px-4 text-gray-800 bg-gray-100 border focus:border-black focus:bg-transparent text-sm pt-3 outline-none transition-all"></textarea>
+                    <input class="text-white bg-black hover:bg-gray-900 tracking-wide text-sm px-4 py-2.5 w-full outline-none" type="submit" value="Send" />
+                </form>
+            </div>
         </div>
     )
 }
