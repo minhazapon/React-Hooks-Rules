6d25d14522ebd04608e1aaf88d88c1fad9c62a0b
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-19 19:35:41 -0800

    added file

--- src/useEffect/useEffect.jsx
+++ src/useEffect/useEffect.jsx
@@ -1,12 +1,23 @@
+import { useEffect } from "react"
+import { useState } from "react"
 
 
 function Effect() {
+
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.log(` the best number is a ${count} `)
+    }, [count])
+
     return (
         <>
-            <div>
+            <div className=" m-20 ">
 
-
-
+                <div className=" flex justify-center items-center gap-5  ">
+                    <p> count: {count} </p>
+                    <button onClick={() => setCount(count + 1)} className=" btn ">Will++</button>
+                </div>
 
             </div>
         </>
