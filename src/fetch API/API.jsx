8e0fbd0f7d4b5e2549aa5c6246923c21a7d8b89f
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-20 05:15:10 -0800

    added file

--- src/fetch API/API.jsx
+++ src/fetch API/API.jsx
@@ -26,17 +26,18 @@ function Api() {
             <>
                 <div className=" ">
                     <div className=" ">
-
                         <div className=" flex justify-center mt-10 ">
-
-                            <div className=" border-[1px] p-5 bg-blue-50 rounded ">
-                                <img className=" h-[300px] " src={pokemonData.sprites.other.dream_world.front_default} alt="" />
+                            <div className=" border-[1px] p-5 bg-blue-50 rounded h-[500px] w-[400px] ">
+                                <div className=" flex justify-center">
+                                    <img className=" h-[300px] " src={pokemonData.sprites.other.dream_world.front_default} alt="" />
+                                </div>
                                 <p className=" text-center mt-5 text-5xl font-bold text-black"> {pokemonData.name} </p>
-                                <div>
-                                    <p></p>
+                                <div className=" flex justify-center items-center gap-5 mt-5 ">
+                                    <p className=" border-[1px] p-2 bg-green-300 rounded-md ">base_stat: {pokemonData.stats[5].base_stat} </p>
+                                    <p className=" border-[1px] p-2 bg-green-300 rounded-md ">effort: {pokemonData.stats[5].effort} </p>
+                                    <p className=" border-[1px] p-2 bg-green-300 rounded-md ">name: {pokemonData.stats[5].stat.name} </p>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
